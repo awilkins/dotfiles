5e991e3eb4f978fa55077d4a10bcc51893af3282
Adrian Wilkins <adrian.wilkins@madetech.com>
2024-08-28 22:54:06 +0100

    Initial commit

---
+++ private_dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.g.python3_host_prog = "/home/awilkins/.asdf/installs/python/3.11.6/bin/python3"
+
+local opt = vim.opt
+
+opt.title = true
+opt.conceallevel = 0
+opt.spelllang = "en_gb"
---
+++ private_dot_config/nvim/lua/plugins/taskjuggler.lua
@@ -0,0 +1,4 @@
+return {
+  "kalafut/vim-taskjuggler",
+  ft = { "tjp", "tji" },
+}
