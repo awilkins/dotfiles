eda65c3e8efff81faf84dc862291bdf1caf66abd
Adrian Wilkins <adrian.wilkins.contractor@dvla.gov.uk>
2024-12-11 16:43:43 +0000

    Disable snacks spelling

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -8,4 +8,20 @@ local opt = vim.opt
 
 opt.title = true
 opt.conceallevel = 0
+opt.spell = false
 opt.spelllang = "en_gb"
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
---
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    win = {
+      spell = { enabled = false },
+    },
+  },
+}
